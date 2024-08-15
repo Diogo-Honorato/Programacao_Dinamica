c31e8a5fedbebb86fdcf918f1ea1dd1ea63e458d
Diogo Augusto Martins Honorato <honoratodiogo097@gmail.com>
2024-08-14 23:29:09 -0300

    Delete TP3 directory

--- TP3/Headers/BMHS.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BHMS_H_
-#define _BHMS_H_
-
-int BMHS(char *texto, char *padrao, int inicio, int fim);
-
-
-
-#endif
--- TP3/Headers/ShiftAnd.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SHIFTAND_H_
-#define _SHIFTAND_H_
-
-int shiftAnd(char *texto, char *padrao, int inicio, int fim);
-
-
-
-#endif
